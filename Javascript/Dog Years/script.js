0bf2c4734814ce761084914389fbbab551c93804
Chuong Vo <vohoang2005@gmail.com>
2021-11-12 15:10:50 -0800

    add Javascript folder

---
+++ Javascript/Dog Years/script.js
@@ -0,0 +1,15 @@
+// This is my current age
+const myAge = 35;
+// This would be the dog age
+let earlyYears = 2;
+// Dog first 2 year
+earlyYears = earlyYears * 10.5;
+// Age - 2
+let laterYears = myAge - 2;
+// Dog after 2 years
+laterYears = laterYears * 4;
+// Figuring out age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+//Using tolowercase method.
+var myName = 'Chuong'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
